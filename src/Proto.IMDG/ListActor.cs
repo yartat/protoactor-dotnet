a6fbf2ab5b60f55ca990eed566de7b2042781faf
rogeralsing <rogeralsing@gmail.com>
2017-11-09 13:54:48 +0100

    imdg

---
+++ src/Proto.IMDG/ListActor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Proto.IMDG.PList;
+
+namespace Proto.IMDG
+{
+    public class ListActor : IActor
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case AddRequest msg:
+                {
+                    var obj = PSerializer.Deserialize(msg.Value);
+                    _list.Add(obj);
+                    break;
+                }
+                case CountRequest _:
+                {
+                    context.Respond(new CountResponse {Value = _list.Count});
+                    break;
+                }
+                case ClearRequest _:
+                {
+                    _list.Clear();
+
+                    break;
+                }
+                case GetRequest msg:
+                {
+                    var obj = _list[msg.Index];
+                    context.Respond(new GetResponse {Value = PSerializer.Serialize(obj)});
+                    break;
+                }
+            }
+
+            return Actor.Done;
+        }
+    }
+}
---
+++ src/Proto.IMDG/PSerializer.cs
@@ -0,0 +1,43 @@
+using System.IO;
+using Google.Protobuf;
+using Proto.IMDG.PList;
+using Wire;
+
+namespace Proto.IMDG
+{
+    public  static class PSerializer
+    {
+        private static readonly Serializer Serializer = new Serializer(new SerializerOptions(false, true));
+        public static PObject Serialize<T>(T item)
+        {
+            using (var ms = new MemoryStream())
+            {
+                Serializer.Serialize(item, ms);
+                ms.Position = 0;
+                return new PObject()
+                {                    
+                    Payload = ByteString.FromStream(ms),
+                };
+            }
+            
+        }
+
+        public static object Deserialize(PObject self)
+        {
+            using (var ms = new MemoryStream(self.Payload.ToByteArray()))
+            {
+                var res = Serializer.Deserialize(ms);
+                return res;
+            }
+        }
+
+        public static T Deserialize<T>(PObject self)
+        {
+            using (var ms = new MemoryStream(self.Payload.ToByteArray()))
+            {
+                var res = Serializer.Deserialize<T>(ms);
+                return res;
+            }
+        }
+    }
+}
