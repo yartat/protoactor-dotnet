d0618b41310a1318461d4c8ad9f9f5397b8fa8ac
rogeralsing <rogeralsing@gmail.com>
2018-06-15 17:23:08 +0200

    I have no idea how Asp.NET Core works

--- examples/AspNetIntegration/Controllers/ActorController.cs
+++ examples/AspNetIntegration/Controllers/ActorController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Proto;
 using AspNetIntegration.Messages;
 
 namespace AspNetIntegration.Controllers
@@ -12,8 +9,15 @@ namespace AspNetIntegration.Controllers
     [ApiController]
     public class ActorController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "hello";
+        }
+        
         [HttpPost("{id}")]
-        public async Task<HelloResponse> Post(string id,[FromBody] HelloRequest request)
+        [ProducesResponseType(typeof(HelloResponse), 201)]
+        public async Task<ActionResult<HelloResponse>> Post(string id,[FromBody] HelloRequest request)
         {
             var (context, pid) = Proto.Http.Extensions.Resolve(id);
             var response = await context.RequestAsync<HelloResponse>(pid, request, TimeSpan.FromSeconds(5));
